79e75f89b6805dd6d3a7b943b72a8950781128a2
brkala <brkala16@gmail.com>
2021-11-11 08:02:18 +0000

    Fix syntax error on error checking

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ use Phalcon\Di\FactoryDefault;
 
 require_once './../vendor/autoload.php';
 
-error_reporting($_ENV['DEBUG'] == 1 ? E_ALL ^ E_WARNING) :0;
+error_reporting($_ENV['DEBUG'] == 1 ? E_ALL ^ E_WARNING:0);
 
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', constant('BASE_PATH') . '/app');
