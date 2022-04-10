09125dac2c7f13553f12f0e7a82f3e1f5fc054e4
brkala <brkala16@gmail.com>
2022-04-10 20:19:14 +0300

    Remove hard coded id from find bonuz bot helper.

--- app/helpers/FindBonuzBot.php
+++ app/helpers/FindBonuzBot.php
@@ -17,25 +17,20 @@ class FindBonuzBot
      */
     static function find($auth)
     {
-        $default_bonuz_bot_id = 37;
-        $bot = Users::findFirst($default_bonuz_bot_id);
-        if(empty($bot) || !$bot->is_admin) {
-            $email = "bonuzbot@bonuz.com";
-            $bot = Users::findFirst(["email=:email:", 'bind' => ['email' => $email]]);
-            if(empty($bot)) {
-                $bot = new Users();
-                $bot->name = 'Bonuz Bot';
-                $bot->status = 0;
-                $bot->email = 'bonuzbot@bonuz.com';
-                $bot->monthly_limit=9999;
-                $bot->is_admin = 1;
-                $bot->password = 'xd';
-                if($bot->create()) {
-                    $bot->password = $auth->hashPassword('123456bonuzbot>@',$bot->id);
-                    $bot->save();
-                }
+        $email = "bonuzbot@bonuz.com";
+        $bot = Users::findFirst(["email=:email:", 'bind' => ['email' => $email]]);
+        if (empty($bot)) {
+            $bot = new Users();
+            $bot->name = 'Bonuz Bot';
+            $bot->status = 0;
+            $bot->email = 'bonuzbot@bonuz.com';
+            $bot->monthly_limit = 9999;
+            $bot->is_admin = 1;
+            $bot->password = 'xd';
+            if ($bot->create()) {
+                $bot->password = $auth->hashPassword('123456bonuzbot>@', $bot->id);
+                $bot->save();
             }
-
         }
         return $bot;
     }
